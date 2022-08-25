59732823e994a470a45c2390d564db407cdf8e10
Sean Patrick Santos <SeanPatrickSantos@gmail.com>
2022-08-25 14:43:34 -0700

    Initial separation of descriptor module.

--- bin_model/transform.py
+++ bin_model/transform.py
@@ -12,7 +12,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-"""Moment transforms used for JEFE"""
+"""Moment transforms used for JEFE."""
 
 from abc import ABC, abstractmethod
 
